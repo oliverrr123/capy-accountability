fa5d177994230919be4c53a66ba6f55dfab091e7
yzde <74788670+yazidears@users.noreply.github.com>
2026-01-29 22:36:02 +0100

    fix: include capyaccountability as normal directory

---
+++ CapySwifty/capyaccountability/capyaccountability/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capyaccountability
+//
+//  Created by Yazide Arsalan on 29/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CapySwifty/capyaccountability/capyaccountability/capyaccountabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  capyaccountabilityApp.swift
+//  capyaccountability
+//
+//  Created by Yazide Arsalan on 29/1/26.
+//
+
+import SwiftUI
+
+@main
+struct capyaccountabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
