571f3a4eadfa88bca90a2ecb0cf1ec28b6eddc89
oliverrr123 <oliver.cingl@gmail.com>
2026-02-02 21:35:16 +0100

    feat: speech screen

---
+++ capyaccountability/Views/SpeechView.swift
@@ -0,0 +1,58 @@
+import AuthenticationServices
+import SwiftUI
+
+struct SpeechView: View {
+    var body: some View {
+        ZStack {
+            Color.capyBlue
+                .ignoresSafeArea()
+            
+            Image("wallpaper")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .opacity(0.5)
+            
+            LinearGradient(
+                colors: [
+                    Color.black.opacity(0.2),
+                    Color.black.opacity(0.1),
+                    Color.black.opacity(0),
+                    Color.black.opacity(0)
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            VStack {
+                                
+                Spacer()
+                
+                
+                VStack {
+                    ZStack {
+                        Image("speech_bubble")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 20)
+                        
+                        Text("What's ur name?")
+                            .font(.custom("Gaegu-Regular", size: 28))
+                            .padding(.bottom, 18)
+                    }
+                    
+                    Image("capy_sit")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.bottom, 10)
+                
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    InitialView()
+}
