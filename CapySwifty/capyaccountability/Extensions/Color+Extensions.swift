97ecb17aac6a1a72d37dfca4e75b1f8d3f1d6d57
yzde <74788670+yazidears@users.noreply.github.com>
2026-02-02 18:18:50 +0100

    stuff and caches

---
+++ CapySwifty/capyaccountability/Extensions/Color+Extensions.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+extension Color {
+    static let capyBlue = Color(red: 0.1, green: 0.6, blue: 1.0) // Approximate bright blue
+    static let capyBeige = Color(red: 0.85, green: 0.78, blue: 0.70) // Button beige
+    static let capyBrown = Color(red: 0.35, green: 0.25, blue: 0.20) // Button brown
+}
