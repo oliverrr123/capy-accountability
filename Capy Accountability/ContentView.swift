5fce79676f3cf4df1f64b989dfa6856e0bc3134b
oliverrr123 <oliver.cingl@gmail.com>
2026-02-02 18:23:57 +0100

    feat: first screen

--- Capy Accountability/ContentView.swift
+++ Capy Accountability/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text("Capy").font(.custom("CherryBombOne-Regular", size: 96)).foregroundStyle(.white)
+            Text("Accountability").font(.custom("CherryBombOne-Regular", size: 32)).foregroundStyle(.white)
+            
+            Spacer()
+            
+            Image("Capy")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .saturation(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 28/255, green: 149/255, blue: 255/255))
+        .ignoresSafeArea()
     }
 }
 
