dd8782cd9d5604244ae87537d6d12887dafb05ee
Hodan <hodan@Hodans-MacBook-Air.local>
2026-02-17 02:18:22 +0100

    fix: secret

--- capyaccountability/LiveActivity/CapyLiveActivityModels.swift
+++ capyaccountability/LiveActivity/CapyLiveActivityModels.swift
@@ -60,23 +60,23 @@ extension CapyLiveActivityGoalScope {
     }
 }
 
-struct CapyLiveActivityAttributes: ActivityAttributes {
-    public struct ContentState: Codable, Hashable {
-        var headline: String
-        var focusText: String
-        var progressText: String
-        var coins: Int
-        var isSleeping: Bool
-        var mood: String
-        var isAway: Bool
-        var mode: String
-        var goalScope: String
-        var needsFood: Bool
-        var isDailyPriority: Bool
-    }
-
-    var profileName: String
-}
+//struct CapyLiveActivityAttributes: ActivityAttributes {
+//    public struct ContentState: Codable, Hashable {
+//        var headline: String
+//        var focusText: String
+//        var progressText: String
+//        var coins: Int
+//        var isSleeping: Bool
+//        var mood: String
+//        var isAway: Bool
+//        var mode: String
+//        var goalScope: String
+//        var needsFood: Bool
+//        var isDailyPriority: Bool
+//    }
+//
+//    var profileName: String
+//}
 
 //struct CapyLiveActivitySnapshot: Equatable {
 //    var profileName: String
